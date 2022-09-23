5d0c2793510a3e316742ab25372bd58d279ebf73
LEONARDO ALVES LEVANDOSKI <levandoski.7907@aluno.pr.senac.br>
2022-09-22 22:13:46 -0300

    Listagem de Nomes/Usando Vetor

---
+++ AlgoritimoListagem.java
@@ -0,0 +1,20 @@
+public class AlgoritimoListagem {
+    public static void main(String[] args) {
+        //Listagem
+
+        String[] listagemDeNomes = new String[10];
+
+        listagemDeNomes[0]="primeiroNome";
+        listagemDeNomes[1]="segundoNome";
+        listagemDeNomes[2]="terceiroNome";
+        listagemDeNomes[3]="quartoNome";
+        listagemDeNomes[4]="quintoNome";
+        listagemDeNomes[5]="sextoNome";
+        listagemDeNomes[6]="setimoNome";
+        listagemDeNomes[7]="oitavoNome";
+        listagemDeNomes[8]="nonoNome";
+        listagemDeNomes[9]="decimoNome";
+
+        System.out.println(listagemDeNomes[4]);
+    }
+}
