a2f5b272e8b97e2ef54b6bbed79907ab38717e8f
LEONARDO ALVES LEVANDOSKI <levandoski.7907@aluno.pr.senac.br>
2022-09-22 21:45:34 -0300

    Vetor

--- Exer4.java
+++ Exer4.java
@@ -1,5 +1,15 @@
 public class Exer4 {
     public static void main(String[] args) {
-        System.out.print("Teste 01");
+        // Vetor
+         Integer[] listaDeInformacoesDeNumerosInteiros = new Integer[6];
+
+         listaDeInformacoesDeNumerosInteiros[0] =0;
+         listaDeInformacoesDeNumerosInteiros[1] =1;
+         listaDeInformacoesDeNumerosInteiros[2] =2;
+         listaDeInformacoesDeNumerosInteiros[3] =3;
+         listaDeInformacoesDeNumerosInteiros[4] =4;
+         listaDeInformacoesDeNumerosInteiros[5] =5;
+
+         System.out.println(listaDeInformacoesDeNumerosInteiros[5]);
 
 }}
