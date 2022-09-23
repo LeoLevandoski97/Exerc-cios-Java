832b8085a8d9ea33447e9eb6e52304a0fcc32685
LEONARDO ALVES LEVANDOSKI <levandoski.7907@aluno.pr.senac.br>
2022-09-22 21:54:34 -0300

    Matriz

---
+++ Matriz.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Matriz {
+    public static void main(String[] args) {
+        //Matriz
+
+        List<String> listaDeInformacoes= new ArrayList<>();
+
+        listaDeInformacoes.add("Carteira");
+        listaDeInformacoes.add("Bone");
+        listaDeInformacoes.add("Lapis");
+        listaDeInformacoes.add("Mouse");
+        listaDeInformacoes.add("Oculos");
+        listaDeInformacoes.add("Lentes");
+
+        System.out.println(listaDeInformacoes);
+    }
+}
